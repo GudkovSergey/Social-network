a7c0c4e4c688c44bb68fcfa6fb9319d3e14ab790
GudkovSergey <gudkow-sergey@mail.ru>
2021-11-01 21:48:23 +0500

    dispatch

--- my-app/src/redux/state.js
+++ my-app/src/redux/state.js
@@ -1,49 +1,65 @@
 
-let rerenderEntireTree = ()=>{
+let store ={
+    _state:  {
+        profilePage : {
+            postData : [
+                {id: 1, message: 'Hi, how are you?', likecount: 5},
+                {id: 2, message: 'It`s my first post', likecount: 10},
+                {id: 3, message: 'Hi, how are you?', likecount: 5},
+                {id: 4, message: 'It`s my first post', likecount: 10},
+                {id: 5, message: 'Hi, how are you?', likecount: 5},
+                {id: 6, message: 'It`s my first post', likecount: 10}
+            ],
+            newPostText : ''    },
+        dialogsPage : {
+            dialogsData: [
+                {id: 1, name: 'Sergey'},
+                {id: 2, name: 'Lora'},
+                {id: 3, name: 'Kseniya'},
+                {id: 4, name: 'Sava'}
+            ],
+            messagesData: [
+                {id: 1, message: 'Hi'},
+                {id: 1, message: 'How are you?'},
+                {id: 1, message: 'Glad to see you'},
+                {id: 1, message: 'Yo'}
+            ]
+        }
+    },
 
-};
-let state = {
-    profilePage : {
-        postData : [
-            {id: 1, message: 'Hi, how are you?', likecount: 5},
-            {id: 2, message: 'It`s my first post', likecount: 10},
-            {id: 3, message: 'Hi, how are you?', likecount: 5},
-            {id: 4, message: 'It`s my first post', likecount: 10},
-            {id: 5, message: 'Hi, how are you?', likecount: 5},
-            {id: 6, message: 'It`s my first post', likecount: 10}
-        ],
-        newPostText : ''    },
-    dialogsPage : {
-        dialogsData: [
-            {id: 1, name: 'Sergey'},
-            {id: 2, name: 'Lora'},
-            {id: 3, name: 'Kseniya'},
-            {id: 4, name: 'Sava'}
-        ],
-        messagesData: [
-            {id: 1, message: 'Hi'},
-            {id: 1, message: 'How are you?'},
-            {id: 1, message: 'Glad to see you'},
-            {id: 1, message: 'Yo'}
-        ]
+    getState(){
+        debugger;
+        return this._state;
+    },
+    rerenderEntireTree() {
+        console.log('State changed');
+    },
+     addNewPost(postMessage) {
+        debugger;
+        let newPost = {
+            id: 5,
+            message: postMessage,
+            likecount: 0
+        };
+        this._state.profilePage.postData.push(newPost);
+        this.rerenderEntireTree(this._state)
+    },
+
+    updateNewPostText(newText) {
+
+        this._state.profilePage.newPostText = newText;
+        this.rerenderEntireTree(this._state)
+    },
+    subscribe(observer) {
+        this.rerenderEntireTree = observer;
     }
 }
-// window.state = state;
- export let addNewPost = (postMessage) => {
-    let newPost = {
-        id: 5,
-        message: postMessage,
-        likecount: 0
-    };
-    state.profilePage.postData.push(newPost);
-    rerenderEntireTree(state)
-}
-export let updateNewPostText = (newText) => {
 
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state)
-}
-export const subscribe = (observer)=>{
-    rerenderEntireTree = observer;
-}
-export default state;
+
+
+
+
+
+
+window.store = store;
+export default store;
