cd28f460b2bc850bc0058b96b8915025adf1d2b0
GudkovSergey <gudkow-sergey@mail.ru>
2022-01-18 16:13:42 +0500

    18012022

--- my-app/src/components/Users/Users.js
+++ my-app/src/components/Users/Users.js
@@ -1,81 +1,59 @@
 import React from "react";
 import us from './users.module.css';
-import * as axios from "axios";
 import userPhoto from '../../assets/png1.png';
 
-class Users extends React.Component {
-    constructor(props) {
-        super(props);
+let Users = (props) => {
+
+
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
+    return (
+        <div>
 
-    componentDidMount() {
+            <div className={us.pageCount}>
+                {pages.map(p => {
+                    return <span className={props.currentPage === p && us.selectedPage} onClick={(e) => {
 
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items);
-                this.props.setTotalUsersCount(response.data.totalCount);
-            });
+                        props.onPageChanged((p));
 
-    }
-    onPageChanged = (pageNumber)=>{
-        this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items);
+                    }}>{p}</span>
+                })}
 
-            });
-    }
-
-    render() {
-        let pagesCount = Math.ceil(this.props.totalUsersCount/this.props.pageSize);
-        let pages = [];
-        for(let i = 1; i<= pagesCount; i++){
-            pages.push(i);
-        }
-        return (
-            <div>
-
-                <div className={us.pageCount} >
-                    {pages.map(p=>{
-                      return  <span className= {this.props.currentPage === p && us.selectedPage}  onClick={(e)=>{
-
-                          this.onPageChanged((p));
-
-                      }}>{p}</span>}) }
-
-                </div>
-                {
-                    this.props.users.map(u => <div key={u.id}>
-                    <span>
-                        <div>
-                            <img src={u.photos.small != null ? u.photos.small: userPhoto} className={us.photo}/>
+            </div>
+            {
+                props.users.map(u => <div className={us.usersProfile} key={u.id}>
+                    <div className={us.userImage}>
+                        <div className={us.image}>
+                            <img src={u.photos.small != null ? u.photos.small : userPhoto} className={us.photo}/>
                         </div>
-                        <div>
-                            {u.followed
-                                ? <button onClick={() => {
-                                    this.props.unfollow(u.id)
+                        <div className={us.followButton}>
+                            {u.followed ? <button onClick={() => {
+                                    props.unfollow(u.id)
                                 }}>Unfollow</button> :
                                 <button onClick={() => {
-                                    this.props.follow(u.id)
+                                    props.follow(u.id)
                                 }}>Follow</button>}
 
                         </div>
-                        <span>
-                            <span>
-                                <div> {u.name}</div>
-                                <div> {u.status}</div>
-                            </span>
-                            <span>
-                                <div>{"u.location.country"}</div>
-                                <div>{"u.location.city"}</div>
-                            </span>
-                        </span>
-                    </span>
+                    </div>
+                    <div className={us.usersItem}>
+                        <div className={us.wrapperNameStatus}>
+                            <div> {u.name}</div>
+                            <div> {u.status}</div>
+                        </div>
+                        <div className={us.location}>
+                            <div>{"u.location.country"}</div>
+                            <div>{"u.location.city"}</div>
+                        </div>
+                    </div>
 
-                    </div>)
-                }
-            </div>)
-    }
+
+                </div>)
+            }
+        </div>)
 }
 
 
